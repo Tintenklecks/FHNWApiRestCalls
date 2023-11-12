5bd55e57125728f3757c36f0c302375cb356700e
Ingo Boehme <ingo.boehme@zuehlke.com>
2023-11-12 19:10:22 +0100

    First Implementation on the UI thread

--- Simple API calls/ContentView.swift
+++ Simple API calls/ContentView.swift
@@ -1,17 +1,42 @@
-// Created 12.11.2023 
+// Created 12.11.2023
 
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: Internal
+
+    let urlString = "https://official-joke-api.appspot.com/random_joke"
+
+    @State private var responseText = "Hello, world!"
+
     var body: some View {
-        VStack {
+        VStack(spacing: 18) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text(responseText)
+                .lineLimit(10)
+            
+            Button("tell me a joke") {
+                guard let url = URL(string: urlString) else {
+                    responseText = "Somethoing went wrong"
+                    return
+                }
+                guard let data = try? Data(contentsOf: url) else {
+                    responseText = "Somethoing went wrong"
+                    return
+                }
+                guard let resultString = String(data: data, encoding: .utf8) else {
+                    responseText = "Somethoing went wrong"
+                    return
+                }
+                responseText = resultString
+            }
         }
-        .padding()
+        
     }
+
 }
 
 #Preview {
