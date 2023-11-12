27f73a6661be88d2061924cd2e9bf09ee0773381
Ingo Boehme <ingo.boehme@zuehlke.com>
2023-11-12 18:34:20 +0100

    first commit

---
+++ Simple API calls/Simple_API_callsApp.swift
@@ -0,0 +1,12 @@
+// Created 12.11.2023 
+
+import SwiftUI
+
+@main
+struct Simple_API_callsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
