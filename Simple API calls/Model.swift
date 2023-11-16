c85e76691ea21edb6cc28460c92203264d60c04d
Ingo Boehme <ingo.boehme@zuehlke.com>
2023-11-16 13:08:53 +0100

    Added API enum

--- Simple API calls/Model.swift
+++ Simple API calls/Model.swift
@@ -2,20 +2,53 @@
 
 import Foundation
 
+// MARK: - API
+
+enum API {
+    case random
+
+    // MARK: Internal
+
+    var urlString: String {
+        switch self {
+        case .random: "https://official-joke-api.appspot.com/random_joke"
+        }
+    }
+    
+    var url: URL {
+        guard let url = URL(string:  urlString) else {
+            fatalError("Something is wrong with the URL \(urlString)")
+        }
+        return url
+    }
+}
+
+class APIService {
+    
+    func fetch(endpoint: API) -> Data? {
+        
+        guard let data = try? Data(contentsOf: endpoint.url) else {
+            return nil
+        }
+        
+        return data
+
+        
+    }
+    
+}
+
+
 class Model {
     var joke: Joke?
-    let urlString = "https://official-joke-api.appspot.com/random_joke"
 
-    func loadData() {
-        guard let url = URL(string: urlString) else {
-            joke = Joke(type: "", setup: "Error", punchline: "Error converting the URL", id: 0)
-            return
-        }
-        guard let data = try? Data(contentsOf: url) else {
+        func loadData()
+    {
+        guard let data = try? Data(contentsOf: API.random.url) else {
             joke = Joke(type: "", setup: "Error", punchline: "Error loading the data from the joke URL", id: 0)
             return
         }
-        guard let joke  = try? JSONDecoder().decode(Joke.self, from: data) else {
+        guard let joke = try? JSONDecoder().decode(Joke.self, from: data) else {
             joke = Joke(type: "", setup: "Error", punchline: "Error loading the data from the joke URL", id: 0)
             return
         }
